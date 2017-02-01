bf962bcdeb9556e6a1c8c7d6692aea0c906390d9
sgoldsmith <sgoldsmith@dx4350>
2017-01-31 23:00:39 -0500

    Updated

--- scripts/install-opencv.sh
+++ scripts/install-opencv.sh
@@ -69,7 +69,7 @@ apt-get -y install opencl-headers libtbb2 libtbb-dev libeigen3-dev libatlas-dev 
 
 cd "$buildhome" >> $logfile 2>&1
 log "Cloning opencv"
-git clone --depth 1 git clone --depth 1 https://github.com/Itseez/opencv.git >> $logfile 2>&1
+git clone --depth 1 https://github.com/Itseez/opencv.git >> $logfile 2>&1
 log "Cloning opencv_contrib"
 git clone --depth 1 https://github.com/Itseez/opencv_contrib.git >> $logfile 2>&1
 
