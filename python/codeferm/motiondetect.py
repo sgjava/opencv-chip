51966267f57b7b6d45ed0df00090c5812f5494a5
sgoldsmith <sgoldsmith@dx4350>
2017-02-14 22:58:56 -0500

    Test

--- python/codeferm/motiondetect.py
+++ python/codeferm/motiondetect.py
@@ -172,10 +172,7 @@ if __name__ == '__main__':
                                         i = 0
                                         for x, y, w, h in foundLocations:
                                             imageRoi2 = imageRoi[y : y + h, x : x + w]
-                                            x2 = imageRoi2[0]
-                                            y2 = imageRoi2[1]
-                                            w2 = imageRoi2[2]
-                                            h2 = imageRoi2[3]
+                                            (x2, y2, w2, h2) = imageRoi2
                                             # Draw rectangle around people
                                             cv2.rectangle(image, (x2 * widthMultiplier, y2 * heightMultiplier),
                                             ((x2 + w2) * widthMultiplier, (y2 + h2) * heightMultiplier), (255, 0, 0), 2)
