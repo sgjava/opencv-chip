64d441389c935a1ba57a26035fbe2fd79fabb991
sgoldsmith <sgoldsmith@dx4350>
2017-02-23 21:04:54 -0500

    Cleaned up

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,6 +6,20 @@
 #
 # Make sure you change this before running any script!
 #
+# Tested configs:
+#
+# OpenCV 3.2 detects NEON, so we leave that out
+#
+# CHIP R8 - TBB build failed, so we use built in one
+#
+# extracflag="-mtune=cortex-a8 -mfloat-abi=hard"
+# cmakeopts="-DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DWITH_QT=OFF -DWITH_GTK=OFF -DWITH_TBB=ON -DBUILD_TBB=OFF -DENABLE_NEON=ON"
+#
+# Pine64 - Carotene fails with "conflicts with asm clobber list", so we disable
+#
+# extra_c_flag="-mtune=cortex-a53"
+# cmakeopts="-DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DWITH_QT=OFF -DWITH_GTK=OFF -DWITH_TBB=ON -DBUILD_TBB=ON -DWITH_CAROTENE=OFF"
+#
 # Steven P. Goldsmith
 # sgjava@gmail.com
 # 
@@ -19,16 +33,8 @@ tmpdir="/media/usb0/temp"
 # Build home
 buildhome="/media/usb0"
 
-# OpenCV 3.2 detects NEON, so we leave that out
+# OpenCV extra cflags
+extracflag="-mtune=cortex-a8 -mfloat-abi=hard"
 
-# Compile for CHIP R8
-# TBB build failed, so we use built in one
-
-#extracflag="-mtune=cortex-a8 -mfloat-abi=hard"
+# Custom cmake options
 cmakeopts="-DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DWITH_QT=OFF -DWITH_GTK=OFF -DWITH_TBB=ON -DBUILD_TBB=OFF -DENABLE_NEON=ON"
-
-# Compile for Pine64 ARM Cortex A53
-# Carotene fails with "conflicts with asm clobber list", so we disable
-
-#extra_c_flag="-mtune=cortex-a53"
-#cmakeopts="-DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DWITH_QT=OFF -DWITH_GTK=OFF -DWITH_TBB=ON -DBUILD_TBB=ON -DWITH_CAROTENE=OFF"
